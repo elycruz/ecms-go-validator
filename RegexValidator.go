1986bf9a27fa5515f56f494afdc11f859fbc0a46
Ely De La Cruz <elycruz@elycruz.com>
2018-12-02 11:56:47 -0500

    regex-validator - Initial regex-validator implementation.

--- RegexValidator.go
+++ RegexValidator.go
@@ -1,12 +1,57 @@
 package ecms_validator
 
-import "regexp"
+import (
+	"fmt"
+	"regexp"
+)
+
+const (
+	DoesNotMatchPattern = iota
+)
 
 type RegexValidatorOptions struct {
-	Pattern regexp.Regexp
+	Pattern *regexp.Regexp
 	MessageTemplates map[int]MessageTemplateFunc
 }
 
-func RegexValidator(options RegexValidatorOptions) (bool, []string) {
-	return false, nil
+var RegexValidatorMessageFuncs MessageTemplateFuncs
+
+func init() {
+	RegexValidatorMessageFuncs = MessageTemplateFuncs{
+		DoesNotMatchPattern: func(options ValidatorOptions, x interface{}) string {
+			ops := options.(RegexValidatorOptions)
+			return fmt.Sprintf("%v is does not match required pattern %v", x, ops.Pattern.String())
+		},
+	}
+}
+
+func NewRegexValidatorOptions () RegexValidatorOptions {
+	return RegexValidatorOptions{
+		Pattern: nil,
+		MessageTemplates: RegexValidatorMessageFuncs,
+	}
+}
+
+func RegexValidator(options RegexValidatorOptions) Validator {
+	return func(x interface{}) (bool, []string) {
+		match := options.Pattern.Match(x.([]byte))
+		if match != true {
+			return false, []string{
+				options.GetErrorMessageByKey(DoesNotMatchPattern, x),
+			}
+		}
+		return true, nil
+	}
+}
+
+func (n RegexValidatorOptions) GetErrorMessageByKey(key int, x interface{}) string {
+	return GetErrorMessageByKey(n, key, x)
+}
+
+func (n RegexValidatorOptions) GetMessageTemplates() MessageTemplateFuncs {
+	return n.MessageTemplates
+}
+
+func (n RegexValidatorOptions) GetValueObscurator() ValueObscurator {
+	return DefaultValueObscurator
 }
